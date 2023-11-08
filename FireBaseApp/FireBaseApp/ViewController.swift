5bffa210a61c1cfbedfda4be901a8864cc04cb2f
KatsiarynaDunets <kate.dunets@gmail.com>
2023-11-08 22:31:31 +0100

    Project Created

---
+++ FireBaseApp/FireBaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseApp
+//
+//  Created by Kate on 08/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
